34b0cdfa1a4752163ff0701871d6992237707716
Yazan Kayed <y.arefkayed@gmail.com>
2023-07-03 21:59:06 +0300

    init commit

---
+++ First Java Program/Info.java
@@ -0,0 +1,9 @@
+public class Info {
+    public static void main(String[] args){
+
+        System.out.println("Hello my name is Yazan\nI am 24 years old\nMy home town in Sabastya, Nablus ");
+    }
+
+
+
+}
