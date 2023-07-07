8e7c31da59d6af93101a374599d3578494a27fae
Yazan Kayed <y.arefkayed@gmail.com>
2023-07-07 15:43:34 +0300

    Bank Account

--- Java OOP/BankAccount/BankAccount.java
+++ Java OOP/BankAccount/BankAccount.java
@@ -3,20 +3,71 @@ public class BankAccount {
     private double checkingBalance;
     private double savingsBalance;
 
-    private static int accounts;
-    private static double totalMoney; // refers to the sum of all bank account checking and savings balances
+    private static int numberOfAccounts=0;
+    private static double totalMoney=0.0; // refers to the sum of all bank account checking and savings balances
 
+    public BankAccount() {
+        this.checkingBalance=0.0;
+        this.savingsBalance=0.0;
+        numberOfAccounts++;
+
+    }
+    
     // CONSTRUCTOR
     // Be sure to increment the number of accounts
 
     // GETTERS
     // for checking, savings, accounts, and totalMoney
-
+    public double getCheckingBalance() {
+        System.out.println("Your Checking Balance = " +this.checkingBalance);
+        return checkingBalance;
+    }
+    public double getSavingsBalance() {
+        System.out.println("Your Saving Balance = " +this.savingsBalance);
+        return savingsBalance;
+    }
+        public double getTotalMoneyInTheAccount() {
+            double totalMoneyInTheAccount=this.savingsBalance+this.checkingBalance;
+        System.out.println("Total money in the bank = " +totalMoneyInTheAccount);
+        return totalMoneyInTheAccount;
+        }
+    public static double getTotalMoney() {
+        System.out.println("Total money in the bank = " +totalMoney);
+        return totalMoney;
+    }
+    public static int getNumberOfAccounts() {
+        System.out.println("Total Accounts in the bank = "+numberOfAccounts);
+        return numberOfAccounts;
+    }
     // METHODS
     // deposit
-    // - users should be able to deposit money into their checking or savings account
-    // withdraw 
-    // - users should be able to withdraw money from their checking or savings account
+    // - users should be able to deposit money into their checking or savings
+    // account
+    public void setCheckingBalance(double amount) {
+        this.checkingBalance += amount;
+        totalMoney += amount;
+        System.out.println("Sucessfully added to Checking account");
+    }
+    public void setSavingsBalance(double amount) {
+        this.savingsBalance += amount;
+        totalMoney += amount;
+        System.out.println("Sucessfully added to Savings account");
+    }
+
+    // withdraw
+    public void withdrawCheckingBalance(double amount){
+        this.checkingBalance -= amount;
+        totalMoney -= amount;
+        System.out.println("Sucessfully withdarwed to Checking account");
+    }
+
+    public void withdrawSavingBalance(double amount){
+        this.savingsBalance -= amount;
+        totalMoney -= amount;
+        System.out.println("Sucessfully withdarwed to Saving account");
+    }
+    // - users should be able to withdraw money from their checking or savings
+    // account
     // - do not allow them to withdraw money if there are insufficient funds
     // - all deposits and withdrawals should affect totalMoney
     // getBalance
--- Java OOP/BankAccount/BankTest.java
+++ Java OOP/BankAccount/BankTest.java
@@ -1,9 +1,21 @@
 public class BankTest {
     public static void main(String[] args){
         // Create 3 bank accounts
-
+        BankAccount account1= new BankAccount();
+        BankAccount account2= new BankAccount();
+        BankAccount account3= new BankAccount();
         // Deposit Test
         // - deposit some money into each bank account's checking or savings account and display the balance each time
+        account1.setCheckingBalance(100);
+        account1.setSavingsBalance(300);
+        account2.setCheckingBalance(1000);
+        account2.setSavingsBalance((5000));
+        account3.getCheckingBalance();
+        account1.getCheckingBalance();
+        account1.getSavingsBalance();
+        BankAccount.getTotalMoney();
+        BankAccount.getNumberOfAccounts();
+        account1.getTotalMoneyInTheAccount();
         // - each deposit should increase the amount of totalMoney
 
         // Withdrawal Test
